83d16237c72c11dfcb9ce96d1ed3adfd61ae25d7
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-13 19:56:35 -0300

    created "missions" page

--- src/components/Missions/Missions.js
+++ src/components/Missions/Missions.js
@@ -1,8 +1,58 @@
 import React from 'react';
 import './Missions.css';
 
-const Missions = () => (
-  <h2>hola missions</h2>
-);
+const Missions = () => {
+  const missionHistory = [
+    {
+      id: 1,
+      mission: 'mission',
+      desciption: 'Description',
+      status: 'Status',
+      empty: 'Empty',
+    },
+    {
+      id: 2,
+      mission: 'mission',
+      desciption: 'Description',
+      status: 'Status',
+      empty: 'Empty',
+    },
+    {
+      id: 3,
+      mission: 'mission',
+      desciption: 'Description',
+      status: 'Status',
+      empty: 'Empty',
+    },
+    {
+      id: 4,
+      mission: 'mission',
+      desciption: 'Description',
+      status: 'Status',
+      empty: 'Empty',
+    },
+  ];
+
+  return (
+    <div className="table">
+      <ul className="tableUl">
+        <li className="tableLi">
+          <h3 className="gridItem">Mission</h3>
+          <h3 className="gridItem">Description</h3>
+          <h3 className="gridItem">Status</h3>
+          <h3 className="gridItem">Empty</h3>
+        </li>
+        {missionHistory.map((miss) => (
+          <li key={miss.id} className="tableLi">
+            <h3 className="gridItem">{miss.mission}</h3>
+            <p className="gridItem">{miss.desciption}</p>
+            <h4 className="gridItem">{miss.status}</h4>
+            <button className="gridItem" type="button">{miss.empty}</button>
+          </li>
+        ))}
+      </ul>
+    </div>
+  );
+};
 
 export default Missions;
