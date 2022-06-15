8c6af09630486a0befa121efeefb53db3155a989
axel soler <99031215+AxelSoler@users.noreply.github.com>
2022-06-15 18:17:47 -0300

    update status with React conditional rendering syntax

--- src/components/Missions/Mission.js
+++ src/components/Missions/Mission.js
@@ -5,33 +5,39 @@ import { joinMission, leaveMission } from '../../redux/missions/missions';
 
 const Mission = (props) => {
   const {
-    mission, description, id,
+    mission, description, id, join,
   } = props;
   const dispatch = useDispatch();
   const [isActive, setActive] = useState('false');
-  const [member, setMember] = useState('NOT A MEMBER');
 
-  const join = (e) => {
+  const joinM = (e) => {
     const missionid = e.target.parentElement.id;
     dispatch(joinMission(missionid));
     setActive(!isActive);
-    setMember('ACTIVE MEMBER');
   };
 
-  const leave = (e) => {
+  const leaveM = (e) => {
     const missionid = e.target.parentElement.id;
     dispatch(leaveMission(missionid));
     setActive(!isActive);
-    setMember('NOT A MEMBER');
   };
 
   return (
     <li id={id} className="tableLi">
       <h3 className="gridItem">{mission}</h3>
       <p className="gridItem descriptionP">{description}</p>
-      <h4 className="gridItem">{member}</h4>
-      <button className={isActive ? 'gridItem' : 'inactive'} type="button" onClick={join}>Join Mission</button>
-      <button className={isActive ? 'inactive' : 'gridItem'} type="button" onClick={leave}>Leave Mission</button>
+      {join && (
+        <h4 className="gridItem">ACTIVE MEMBER</h4>
+      )}
+      {!join && (
+        <h4 className="gridItem">NOT A MEMBER</h4>
+      )}
+      {!join && (
+        <button className="gridItem" type="button" onClick={joinM}>Join Mission</button>
+      )}
+      {join && (
+        <button className="gridItem" type="button" onClick={leaveM}>Leave Mission</button>
+      )}
     </li>
   );
 };
@@ -42,4 +48,7 @@ Mission.propTypes = {
   mission: PropTypes.string.isRequired,
   description: PropTypes.string.isRequired,
   id: PropTypes.string.isRequired,
+  join: PropTypes.bool,
 };
+
+Mission.defaultProps = { join: false };
