14d0e5d841719f51ba4d3e5953b917e083bca634
Rafael Guzman <rega1237@gmail.com>
2022-06-15 20:14:44 -0400

    Call the store and filter the state to get only the reserved rockets and display on the screen

--- src/components/MyProfile/MyProfile.js
+++ src/components/MyProfile/MyProfile.js
@@ -3,14 +3,30 @@ import { useSelector } from 'react-redux';
 
 const MyProfile = () => {
   const listOfMissions = useSelector((state) => state.listOfMissions);
+  const listRockets = useSelector((state) => state.rockets);
   const allMissions = listOfMissions.filter((mission) => mission.join === true);
+  const listActiveRockets = listRockets.filter((rocket) => rocket.reserved === true);
 
   return (
     <div className="container">
-      <h3>Missions Joinned</h3>
-      <div>
-        {allMissions.map((miss) => (
-          <div key={miss.id}><h4>{miss.mission}</h4></div>))}
+      <div className="rockets-joined">
+        <h3>Rockets Joined</h3>
+        <div className="active-rockets">
+          <ul>
+            {
+              listActiveRockets.map((rocket) => (
+                <div key={rocket.id}>{rocket.name}</div>
+              ))
+            }
+          </ul>
+        </div>
+      </div>
+      <div className="missions-joined">
+        <h3>Missions Joined</h3>
+        <div>
+          {allMissions.map((miss) => (
+            <div key={miss.id}><h4>{miss.mission}</h4></div>))}
+        </div>
       </div>
     </div>
   );
